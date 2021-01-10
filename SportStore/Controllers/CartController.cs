4a7b06b72437502c634bb50b2616eadb0d2afdf7
Dmytro Ileiko <Dmytro.Ileiko@ist.com>
2021-01-10 13:00:05 +0200

    Finished COntrollers section

---
+++ SportStore/Controllers/CartController.cs
@@ -0,0 +1,88 @@
+﻿using DBService.Models;
+using DBService.Repositories;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.Logging;
+
+using SportStore.Infrastructure;
+using SportStore.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportStore.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ILogger<CartController> _logger;
+        private IRepository productRepository;
+        private IOrdersRepository ordersRepository;
+
+        public CartController(ILogger<CartController> logger, IRepository productRepository, IOrdersRepository ordersRepository)
+        {
+            _logger = logger;
+            this.productRepository = productRepository;
+            this.ordersRepository = ordersRepository;
+        }
+
+        public IActionResult Index(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View(GetCart());
+        }
+
+        [HttpPost]
+        public IActionResult AddToCart(Product product, string returnUrl)
+        {
+            SaveCart(GetCart().AddItem(product, 1));
+            return RedirectToAction(nameof(Index), new { returnUrl });
+        }
+
+        [HttpPost]
+        public IActionResult RemoveFromCart(long productId, string returnUrl)
+        {
+            SaveCart(GetCart().RemoveItem(productId));
+            return RedirectToAction(nameof(Index), new { returnUrl });
+        }
+
+        public IActionResult CreateOrder()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateOrder(Order order)
+        {
+            order.Lines = GetCart().Selections.Select(s => new OrderLine
+            {
+                ProductId = s.ProductId,
+                Quantity = s.Quantity
+            }).ToArray();
+            ordersRepository.AddOrder(order);
+            SaveCart(new Cart());
+            return RedirectToAction(nameof(Completed));
+        }
+
+        public IActionResult Completed() => View();
+
+        private Cart GetCart() =>
+            HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
+
+        private void SaveCart(Cart cart) =>
+            HttpContext.Session.SetJson("Cart", cart);
+
+        public IViewComponentResult Invoke(ISession session)
+        {
+            return new ViewViewComponentResult()
+            {
+                ViewData = new ViewDataDictionary<Cart>(ViewData,
+                    session.GetJson<Cart>("Cart"))
+            };
+        }
+    }
+}
---
+++ SportStore/Controllers/StoreController.cs
@@ -0,0 +1,38 @@
+﻿using DBService.Repositories;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using PagingSortingTools;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        private readonly ILogger<StoreController> _logger;
+        private IRepository productRepository;
+        private ICategoryRepository categoryRepository;
+
+        public StoreController(ILogger<StoreController> logger, IRepository productRepository, ICategoryRepository categoryRepository)
+        {
+            _logger = logger;
+            this.productRepository = productRepository;
+            this.categoryRepository = categoryRepository;
+        }
+
+        public IActionResult Index([FromQuery(Name = "options")]
+                QueryOptions productOptions,
+                QueryOptions catOptions,
+                long category)
+        {
+            ViewBag.Categories = categoryRepository.GetCategories(catOptions);
+            ViewBag.SelectedCategory = category;
+            return View(productRepository.GetProducts(productOptions, category));
+        }
+    }
+}
